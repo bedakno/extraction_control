01122e970a7f8694ab5c15d3fd6c018d6b731829
bedakno <s6beknop@uni-bonn.de>
2022-02-10 13:27:48 +0100

    ADD: libraries for controller

--- ArdLib/SerialComm/SerialComm.cpp
+++ ArdLib/SerialComm/SerialComm.cpp
@@ -16,43 +16,44 @@ SerialComm::SerialComm(){
 }
 
 char* SerialComm::process(char* cmd, size_t cmdlen){
-    uint32_t val=0;
     if(cmdlen<2){ //command has to consist of more than 1 char
         return createCommand(ERR,ERR,1,1);
     }
-    if(cmd[1]==GET){
+    uint32_t val=0;
+    char valArr[cmdlen-2];
+    for(size_t c = 0; c<cmdlen-2; c++){
+        valArr[c] = cmd[c+2];
+    }
+    if(cmd[0] == READ){
         if(cmdlen>2){
             return createCommand(ERR,ERR,2,1);
         }
-        val = getter(cmd[0]);
+        val = read(cmd[1]);
         size_t numlen = ((bool) val * (size_t) log10(val) + 1); //get number of digits from val
-        return createCommand(cmd[0], SET, val, numlen);
+        return createCommand(WRITE, cmd[1], val, numlen);
     }
     
-    if(cmd[1]==SET){
+    if(cmd[0] == WRITE){
         if(cmdlen==2){ //set should have more than 2 chars
-            return createCommand(cmd[0], GET, 0, 0);
+            return createCommand(READ, cmd[1], 0, 0);
         }
-        uint32_t intvalue = getValue(cmd, cmdlen); //get the uint32_t from passed char[]
-        setter(cmd[0], intvalue); //set value
-        val = getter(cmd[0]); //get new value
-        size_t numlen = ((bool) val * (size_t) log10(val) + 1);
-        return createCommand(cmd[0], SET, val, numlen); //output new value
+        //uint32_t intvalue = getValue(cmd, cmdlen); //get the uint32_t from passed char[]; use atoi instead?
+        uint32_t intvalue = atoi(valArr);
+        write(cmd[1], intvalue); //set value
+        val = read(cmd[1]); //get new value
+        size_t _vallen = ((bool) val * (size_t) log10(val) + 1);
+        return createCommand(WRITE, cmd [1], val, _vallen); //output new value
     }
-    
-    if(cmd[1]==EXEC){
-        if(cmd[0]=='R'){ //Reset
-            NVIC_SystemReset();
-        }
-        if(cmd[0]=='C'){ //Check
-            return createCommand(cmd[0],EXEC,42,2);
-        }
-        return createCommand(ERR,EXEC,0,1);
+    if(cmd[0] == RESET){
+        NVIC_SystemReset();
+    }
+    if(cmd[0] == CHECK){ //Check
+        return createCommand(CHECK,CHECK,42,2);
     }
     return createCommand(ERR,ERR,0,1); //error
 }
 
-void SerialComm::reset_input_buffer(){
+void SerialComm::resetInputBuffer(){
     while(Serial.available()){
         Serial.read();
       }
@@ -65,7 +66,7 @@ char* SerialComm::createCommand(char _con, char _delim, uint32_t _val, size_t _n
     cmd[1]=_delim; //2nd char is delimiter
     if(_numlen>0){
         itoa(_val, num_char, 10); //transform given int _val in base 10 to array of chars num_char
-        for(uint8_t c = 0; c<_numlen; c++){
+        for(size_t c = 0; c<_numlen; c++){
             cmd[c+2]=num_char[c]; //write numchar into cmd at according places
         }
     }
@@ -74,7 +75,7 @@ char* SerialComm::createCommand(char _con, char _delim, uint32_t _val, size_t _n
     return cmd;
 }
 
-uint32_t SerialComm::getter(char cmd){
+uint32_t SerialComm::read(char cmd){
     if(cmd=='P'){
         return _p;
     }
@@ -93,7 +94,7 @@ uint32_t SerialComm::getter(char cmd){
     return 0;
 }
 
-void SerialComm::setter(char cmd, uint32_t value){
+void SerialComm::write(char cmd, uint32_t value){
     _p = _p*(cmd!='P')+value*(cmd=='P');
     _i = _i*(cmd!='I')+value*(cmd=='I');
     _d = _d*(cmd!='D')+value*(cmd=='D');
@@ -101,9 +102,9 @@ void SerialComm::setter(char cmd, uint32_t value){
     
 }
 
-uint32_t SerialComm::getValue(char cmd[], uint8_t cmdlen){
+uint32_t SerialComm::readValue(char cmd[], uint8_t cmdlen){
     uint32_t value=0;
-    for(uint8_t nc=cmdlen-1; nc>1; nc--){
+    for(size_t nc=cmdlen-1; nc>1; nc--){ //third value (2) is where number starts
         value+=((cmd[nc]-48)*pow(10,(cmdlen-1)-nc)); //cmd[nc]-48 converts ascii code numbers (from 48 = 0 to 57 = 9); multiplies with 10^0 for nc = cmdlen-1 (last char) and 10^cmdlen-2 (third char) 1st and 2nd char are constant and delimiter
     }
     return value;
--- ArdLib/SerialComm/SerialComm.h
+++ ArdLib/SerialComm/SerialComm.h
@@ -17,20 +17,21 @@ public:
     SerialComm();
     ~SerialComm();
     char* process(char*, size_t);
-    void reset_input_buffer();
+    void resetInputBuffer();
     
 private:
-    static const char GET = '?';
-    static const char SET = '=';
-    static const char EXEC = '!';
+    static const char READ = 'R';
+    static const char WRITE = 'W';
+    static const char RESET = 'X';
+    static const char CHECK = 'C';
     static const char ERR = 'E';
     static const char NL = '\n';
     static const char TER = '\r';
     
 private:
-    void setter(char, uint32_t);
-    uint32_t getter(char);
-    uint32_t getValue(char[], uint8_t);
+    void write(char, uint32_t);
+    uint32_t read(char);
+    uint32_t readValue(char[], uint8_t);
     char* createCommand(char, char, uint32_t, size_t);
 };
 
