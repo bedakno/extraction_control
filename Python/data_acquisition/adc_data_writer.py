27d673a50d166e71ec332f41aff83904dd27b98d
bedakno <s6beknop@uni-bonn.de>
2022-03-07 11:46:03 +0100

    ENH: Small enhances in daq

--- Python/data_acquisition/adc_data_writer.py
+++ Python/data_acquisition/adc_data_writer.py
@@ -6,7 +6,7 @@ class ADCDataWriter(DataWriter, ADCBoard):
 
     columns = 'tstamp L R U D S_H S_V'.split()
 
-    def __init__(self, outfile, comments = ''):
+    def __init__(self, outfile='output', comments = ''):
         """instance to read adc values continously and matching them to a timestamp
 
         Args:
@@ -14,19 +14,19 @@ class ADCDataWriter(DataWriter, ADCBoard):
             comments (str, optional): _description_. Defaults to ''.
         """
         ADCBoard.__init__()
-        DataWriter.__init__(outfile=outfile, columns=self.columns, comments=comments)
+        DataWriter.__init__(outfile=''.join([outfile,'.csv']), columns=self.columns, comments=comments)
         #setup adc channels 7,6,5,4 to common ground as well as 1,2 and 0,2 as differential inputs to arduino ground
         self.setup_channels([7,6,5,4,(1,2),(0,2)])
         self.time0 = time()
 
     def _get_values(self):
-        """reads adc values matching the columns as well as the current timestamp
-        
+        """reads adc values matching the columns and adds a timestamp to them
+
         Returns:
             _type_: dictionary with column name and matching value
         """
         values = self.read_channels(self.columns[1:])
-        timestamp = {'tstamp': time()-self.time0}
+        timestamp = {self.columns[0]: time()-self.time0}
         values.update(timestamp)
         return values
 
