4f85a4b75d27742842644a0a1ad7927bd595c21b
bedakno <s6beknop@uni-bonn.de>
2022-03-07 11:45:41 +0100

    ADD: Code for arduino

--- ArdLib/Controller/Controller.ino
+++ ArdLib/Controller/Controller.ino
@@ -1,14 +1,13 @@
 #include <FastPID.h>
-#include <SPI.h>
-#include <Fast_IO_Due.h>
+#include <Fast_IO_Due.h> 
 #include <SerialComm.h>
 
 
 Fast_IO_Due fastIO;
 SerialComm SCom;
 
-float Hz=160000;
-float Kp=1.2, Ki=0*Hz, Kd=0/Hz;
+float Hz=130000;
+float Kp=0, Ki=1, Kd=0;
 int output_bits=11;
 bool output_signed=true;
 FastPID myPID(Kp, Ki, Kd, Hz, output_bits, output_signed);
@@ -23,18 +22,9 @@ void checkPID(){
 }
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100); // wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100); // wait for a second
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
-  fastIO.initialize_adc({0,1,11});
+  fastIO.initialize_adc({0,1});
   fastIO.initialize_dac();
   checkPID();
   myPID.setOutputRange(-2047,2047);
@@ -43,33 +33,22 @@ void setup() {
 
 int16_t output, setpoint=0;
 bool control;
+
 void controlADD(){
   control = fastIO.calc_norm(5);
   if(control){
-    output=myPID.step(setpoint, IOnorm); //If PID-Controller is turned on (control == true), calculate new output.
+    output=myPID.step(setpoint, fastIO.IOnorm); //If PID-Controller is turned on (control == true), calculate new output.
   }
-  fastIO.write_dac(output+2047); //return output (can be old held one or new calculated one).
+  fastIO.write_dac(int16_t(2047+output)); //return output (can be old held one or new calculated one).
 }
 
-
-void getcommand(){
-  char cmdraw[16];
-  size_t cmdlen = Serial.readBytesUntil('\n', cmdraw, 16);
-  char cmd[cmdlen];
-  for(size_t i =0; i<cmdlen; i++){
-     cmd[i]=cmdraw[i];
-  }
-  char* ans = (char*) malloc((16)*sizeof(char));
-  ans = SCom.process(cmd, cmdlen);
-  Serial.print(ans);
-  Serial.flush();
-  SCom.resetInputBuffer();
-  free(ans);
-}
-
-int start;
-void loop() {    
+void loop() {
+  //int start = micros();
+  //for(size_t t = 0; t<1000000; t++){
   if(Serial.available()){
-    getcommand();
+    SCom.process(myPID, fastIO);
   }
+  controlADD();
+  //}
+  //Serial.println(micros()-start);
 }
