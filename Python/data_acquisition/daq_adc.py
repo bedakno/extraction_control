5cfa18bbdf2d372e1724750f80772061c3dca928
bedakno <s6beknop@uni-bonn.de>
2022-03-07 15:28:13 +0100

    ENH: bug fixes

--- Python/data_acquisition/daq_adc.py
+++ Python/data_acquisition/daq_adc.py
@@ -5,11 +5,11 @@ from adc_board import ADCBoard
 def read_continously(outputfile, channel_numbers, channel_names, **data_writer_kwargs):
     adc = ADCBoard()
     writer = DataWriter(outfile=outputfile, columns=channel_names, **data_writer_kwargs)
-    adc.setup_channels(channel_numbers)
+    adc.setup_channels(channel_numbers[1:])
     with writer as w:
         try:
             while True:
-                result = adc.read_channels(channel_names)
+                result = adc.read_channels(channel_names[1:])
                 w.write_row(tstamp=time(), **result)
         except KeyboardInterrupt:
             pass
@@ -19,4 +19,4 @@ if __name__ == "__main__":
     channel_names = 'tstamp L R U D S_H S_V'.split()
     channel_numbers = [7,6,5,4,(1,2),(0,2)]
     outputfile = 'test.csv'
-    read_continously(channel_names=channel_names, channel_numbers=channel_numbers, outputfile=outputfile)
+    read_continously(channel_names=channel_names, channel_numbers=channel_numbers, outputfile=outputfile, comments = '')
